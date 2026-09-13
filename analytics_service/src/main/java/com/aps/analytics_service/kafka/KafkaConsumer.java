1246d6c92df8bda34921574eb83e572b42e8beaa
Aman Pal Singh <aps@APS-2.local>
2026-09-13 16:45:33 +0530

    Implemted api_gateway and added routes

---
+++ analytics_service/src/main/java/com/aps/analytics_service/kafka/KafkaConsumer.java
@@ -0,0 +1,27 @@
+package com.aps.analytics_service.kafka;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
+
+@Service
+public class KafkaConsumer {
+  private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+
+  @KafkaListener(topics = "patient", groupId = "analytics_service")
+  public void consumeEvent(byte[] event) {
+    try {
+      PatientEvent patientEvent = PatientEvent.parseFrom(event);
+      log.info(
+          "Received Patient Event: [PatientId = {}, PatientName = {}, PatientEmail = {}]",
+          patientEvent.getPatientId(),
+          patientEvent.getPatientName(),
+          patientEvent.getEmail());
+    } catch (InvalidProtocolBufferException e) {
+      log.error("Error deserializing event: {}", e.getMessage());
+    }
+  }
+}
--- patient_service/src/main/java/com/aps/patient_service/service/PatientService.java
+++ patient_service/src/main/java/com/aps/patient_service/service/PatientService.java
@@ -5,72 +5,77 @@ import com.aps.patient_service.dto.PatientResponseDTO;
 import com.aps.patient_service.exception.EmailAlreadyExistsException;
 import com.aps.patient_service.exception.PatientNotFoundException;
 import com.aps.patient_service.grpc.BillingServiceGrpcClient;
+import com.aps.patient_service.kafka.KafkaProducer;
 import com.aps.patient_service.mapper.PatientMapper;
 import com.aps.patient_service.model.Patient;
 import com.aps.patient_service.repository.PatientRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class PatientService {
 
-    private final PatientRepository patientRepository;
-    private final BillingServiceGrpcClient billingServiceGrpcClient;
+  private final PatientRepository patientRepository;
+  private final BillingServiceGrpcClient billingServiceGrpcClient;
+  private final KafkaProducer kafkaProducer;
 
-    @Autowired
-    public PatientService(PatientRepository patientRepository, BillingServiceGrpcClient billingServiceGrpcClient) {
-        this.patientRepository = patientRepository;
-        this.billingServiceGrpcClient = billingServiceGrpcClient;
+  @Autowired
+  public PatientService(
+      PatientRepository patientRepository,
+      BillingServiceGrpcClient billingServiceGrpcClient,
+      KafkaProducer kafkaProducer) {
+    this.patientRepository = patientRepository;
+    this.billingServiceGrpcClient = billingServiceGrpcClient;
+    this.kafkaProducer = kafkaProducer;
+  }
+
+  public List<PatientResponseDTO> getPatients() {
+    List<Patient> allPatients = patientRepository.findAll();
+    List<PatientResponseDTO> patientResponseDTOs =
+        allPatients.stream().map(PatientMapper::toPatientResponseDTO).collect(Collectors.toList());
+    //      List<PatientResponseDTO> patientResponseDTOs = allPatients.stream().map(patient ->
+    // PatientMapper.toPatientResponseDTO(patient)).toList();
+    return patientResponseDTOs;
+  }
+
+  public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO) {
+    if (patientRepository.existsByEmail(patientRequestDTO.getEmail())) {
+      throw new EmailAlreadyExistsException(
+          "Patient with this email already exists " + patientRequestDTO.getEmail());
     }
 
-    public List<PatientResponseDTO> getPatients() {
-        List<Patient> allPatients = patientRepository.findAll();
-        List<PatientResponseDTO> patientResponseDTOs = allPatients.stream().map(PatientMapper::toPatientResponseDTO).collect(Collectors.toList());
-//      List<PatientResponseDTO> patientResponseDTOs = allPatients.stream().map(patient -> PatientMapper.toPatientResponseDTO(patient)).toList();
-        return patientResponseDTOs;
+    Patient patient = patientRepository.save(PatientMapper.toPatient(patientRequestDTO));
+    billingServiceGrpcClient.createBillingAccount(
+        patient.getId().toString(), patient.getName(), patient.getEmail());
+    kafkaProducer.sendEvent(patient);
+    return PatientMapper.toPatientResponseDTO(patient);
+  }
 
+  public PatientResponseDTO updatePatient(UUID id, PatientRequestDTO patientRequestDTO) {
+    // If patient not found
+    Patient patient =
+        patientRepository
+            .findById(id)
+            .orElseThrow(() -> new PatientNotFoundException("Patient id not found " + id));
+
+    // Email is unique across users, but UPDATE logic must exclude the current user.
+    if (patientRepository.existsByEmailAndIdNot(patientRequestDTO.getEmail(), id)) {
+      throw new EmailAlreadyExistsException(
+          "Patient with this email already exists " + patientRequestDTO.getEmail());
     }
+    patient.setName(patientRequestDTO.getName());
+    patient.setEmail(patientRequestDTO.getEmail());
+    patient.setAddress(patientRequestDTO.getAddress());
+    patient.setDateOfBirth(patientRequestDTO.getDateOfBirth());
+    Patient updatedPatient = patientRepository.save(patient);
 
-    public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO) {
-        if (patientRepository.existsByEmail(patientRequestDTO.getEmail())) {
-            throw new EmailAlreadyExistsException("Patient with this email already exists " + patientRequestDTO.getEmail());
-        }
+    return PatientMapper.toPatientResponseDTO(updatedPatient);
+  }
 
-        Patient patient = patientRepository.save(PatientMapper.toPatient(patientRequestDTO));
-        billingServiceGrpcClient.createBillingAccount(patient.getId().toString(), patient.getName(), patient.getEmail());
-        return PatientMapper.toPatientResponseDTO(patient);
-
-    }
-
-    public PatientResponseDTO updatePatient(UUID id, PatientRequestDTO patientRequestDTO) {
-        //If patient not found
-        Patient patient = patientRepository.findById(id).orElseThrow(() -> new PatientNotFoundException("Patient id not found "+ id));
-
-        //Email is unique across users, but UPDATE logic must exclude the current user.
-        if (patientRepository.existsByEmailAndIdNot(patientRequestDTO.getEmail(), id)) {
-            throw new EmailAlreadyExistsException("Patient with this email already exists " + patientRequestDTO.getEmail());
-        }
-        patient.setName(patientRequestDTO.getName());
-        patient.setEmail(patientRequestDTO.getEmail());
-        patient.setAddress(patientRequestDTO.getAddress());
-        patient.setDateOfBirth(patientRequestDTO.getDateOfBirth());
-        Patient updatedPatient= patientRepository.save(patient);
-
-        return PatientMapper.toPatientResponseDTO(updatedPatient);
-    }
-    public void deletePatient(UUID id){
-        patientRepository.deleteById(id);
-    }
+  public void deletePatient(UUID id) {
+    patientRepository.deleteById(id);
+  }
 }
-
-
-
-
-
-
